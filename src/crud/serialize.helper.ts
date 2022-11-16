bcb9dfb28541b21f2b2e33f2d41a3eff60695adf
Dan Jung <dan.jung@curia.ai>
2022-11-16 09:15:40 -0600

    add recursion to join entity

--- src/crud/serialize.helper.ts
+++ src/crud/serialize.helper.ts
@@ -49,47 +49,44 @@ export class SerializeHelper {
     const allRelations = getMetadataArgsStorage().relations;
     const allTargets = [...new Set(allRelations.map((relation) => relation.target))];
 
-    const objRelations = allRelations.filter((relation) => relation.target === dto);
-
-    const related: any = objRelations.map((relation) => relation.propertyName);
-
-    class SimpleResponseDto extends OmitType(dto, related) {}
-    Object.defineProperty(SimpleResponseDto, 'name', {
-      writable: false,
-      value: `${resourceName}ResponseDto`,
-    });
-    // Strip out any relations
     // create a object to hold the Classes
 
     const models: any = {};
+    const recursiveCreateDto = function (dto: any, resourceName: string, joinTree: any): any {
+      const objRelations = allRelations.filter((relation) => relation.target === dto);
 
-    // for any relations in the join options, add them to the dto
-    const relatedKeys = Object.keys(joinTree);
-    if (relatedKeys.length > 0) {
-      const rel = objRelations.find((r) => r.propertyName === relatedKeys[0]);
-      if (rel) {
-        const targetClass: any = allTargets.find((t: any) => t.name.toLowerCase() === relatedKeys[0]);
-        if (targetClass) {
-          class RelatedResponseDto extends targetClass {}
-          Object.defineProperty(RelatedResponseDto, 'name', {
-            writable: false,
-            value: `${resourceName}${relatedKeys[0]}ResponseRelatedDto`,
-          });
-          models[RelatedResponseDto['name']] = RelatedResponseDto;
+      const related: any = objRelations.map((relation) => relation.propertyName);
 
-          ApiPropertyProg(
-            { type: RelatedResponseDto, required: false },
-            SimpleResponseDto.prototype,
-            relatedKeys[0] + 'jiggle',
-          );
+      // Strip out any relations
+      class JoinedResponseDto extends OmitType(dto, related) {}
+      Object.defineProperty(JoinedResponseDto, 'name', {
+        writable: false,
+        value: `${resourceName}ResponseDto`,
+      });
+
+      models[resourceName] = JoinedResponseDto;
+      const relatedKeys = Object.keys(joinTree);
+      relatedKeys.forEach((key) => {
+        const relatedObj = objRelations.find((relation) => relation.propertyName === key);
+        if (relatedObj && typeof relatedObj.type === 'function') {
+          //const relatedObjName = relatedObj.type.name;
+          //const relatedObjType = allTargets.find((target) => target.name === relatedObjName);
+          const relatedObjType = relatedObj.type();
+          if (relatedObjType) {
+            const modelName = `${resourceName}Joined${relatedObjType.name}`;
+            recursiveCreateDto(relatedObjType, modelName, joinTree[key]);
+            // Add in the joined object to the api property
+            ApiPropertyProg({ type: models[modelName], required: false }, JoinedResponseDto.prototype, key + 'jiggle');
+          }
         } else {
-          console.log('targetClass not found', relatedKeys[0]);
+          // We did not find the join in the entity
+          console.log(`Could not find join ${key} in ${resourceName}`);
         }
-      } else {
-        console.log('No relation found for ', relatedKeys[0]);
-      }
-    }
-    models.get = SimpleResponseDto;
+      });
+    };
+
+    recursiveCreateDto(dto, resourceName, joinTree);
+    models.get = models[`${resourceName}ResponseDto`];
     return models;
   }
 }
